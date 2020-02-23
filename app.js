442d3ed87608561f62814d81a2c99e878c4f33fd
rv99 <raghavparsai2002@gmail.com>
2020-02-24 04:40:25 +0530

    deploy-1

--- app.js
+++ app.js
@@ -6,9 +6,9 @@ const app = express()
 const ejs = require("ejs")
 
 const port = process.env.PORT;
-if(port==null || port="")
-{  port=3000;
-}
+// if(port==null || port="")
+// {  port=3000;
+// }
 
 
 //****************************mongoBD connections*************************
