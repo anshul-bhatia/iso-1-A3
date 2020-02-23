e4a08f8995f5cf0ab8581102f5805b3b1f4f687d
rv99 <raghavparsai2002@gmail.com>
2020-02-24 04:22:59 +0530

    initial-deploy

--- app.js
+++ app.js
@@ -1,112 +1,133 @@
 const bodyParser = require("body-parser");
 const express = require("express")
 const mongoose = require("mongoose")
+const geolib = require("geolib")
 const app = express()
 const ejs = require("ejs")
+
+//****************************mongoBD connections*************************
+mongoose.connect("mongodb+srv://admin-raghav:isohack-1@cluster0-zqtcb.mongodb.net/locationDB", {
+  useNewUrlParser: true
+});
 
 app.use(bodyParser.urlencoded({
   extended: true
 }));
+app.use(express.static('static'));
 app.use(express.json()) //to get coordinate working in JSON format
 
 app.set('view engine', 'ejs');
-
-//****************************mongoBD connections*************************
-mongoose.connect("mongodb://localhost:27017/locationDB", {
-  useNewUrlParser: true
-});
-
-//*******************************schema*********************************
-const locationSchema = new mongoose.Schema({
-  longitude: Number,
-  latitude: Number,
-  capacity: Number
-});
-
-//*****************************model*****************************
-const LocationDB = mongoose.model("Location", locationSchema);
-
 
 
 app.listen(3000, function() {
   console.log("okee");
 })
 
-//********************************get requests**********************************
-app.get("/", function(req, res) {
-  LocationDB.find(function(err, locations) {
-    if (!err) {
-      res.render("index", {
-        locations: locations
-      });
-    } else {
-      console.log(err);
-    }
-  })
+//*******************************schema*********************************
+const truckSchema = new mongoose.Schema({
+  longitude: Number,
+  latitude: Number,
+  capacity: Number,
+  date:String,
+  phone:String
+});
+
+const farmerSchema = new mongoose.Schema({
+  longitude: Number,
+  latitude: Number,
+  capacity: Number,
+  date:String,
+  phone:String
 })
-var arr_lon=[]
-var arr_lat=[]
-app.get('/chart',(req,res)=>{
-  // LocationDB.find({},(req,res)=>{
-  //   console.log(res);
-  // });
-  LocationDB.find(function(err,locations){
-    if(err){
-      console.log(err);
-    }else{
-      locations.forEach(function(location){
-        //console.log(location);
-        arr_lon.push(location.longitude);
-        arr_lat.push(location.latitude);
-      })
-    }
-    console.log(arr_lat[0]);
-  })
 
+//*****************************model*****************************
+const truckDB = mongoose.model("TruckInfo", truckSchema);
+const farmerDB = mongoose.model("FarmerInfo",farmerSchema);
+
+
+
+
+
+//*************************************get req******************************
+app.get("/", function(req, res){
+  res.render("index")
 })
-//*******************************post requests**********************************
-app.post("/offer", function(req, res) {
-  // console.log(req.body);
-  const data = new LocationDB({
-    longitude: req.body.lon,
-    latitude: req.body.lat
+
+app.get('/find_vehicle',(req,res)=>{
+    res.render('formFarmer.ejs')
+    console.log("Home")
+});
+app.get('/offer_vehicle',(req,res)=>{
+  res.render('formTruck.ejs')
+});
+
+
+
+//*************************************post req******************************
+
+app.post("/farmer",function(req,res){
+  var availableVehicle=[];
+  //console.log(availableVehicle)
+  console.log("date "+req.body.date);
+  const farmer = new farmerDB({
+    longitude:req.body.longitude,
+    latitude:req.body.latitude,
+    capacity:req.body.capacity,
+    phone:req.body.phone,
+    date:req.body.date
   })
+  farmer.save();
+  console.log("saved details successfully");///////////////////////////////////
 
-  data.save(function(err, ans) {
-    console.log(ans);
-  })
-  res.redirect("/");
-})
-// function deg2rad(deg) {
-//   return deg * (Math.PI/180)
-// }
-
-
-
-app.post("/request",function(req, res) {
-  // function getDistanceFromLatLonInKm(lat1,lon1,lat2,lon2)
-  // console.log(req.body);
-  let lat1 = req.body.lat;
-  let lon1 = req.body.lon;
-  LocationDB.find(function(err,locations){
+  let lat1 = (req.body.latitude)*(Math.PI/180)
+  let lon1 = (req.body.longitude)*(Math.PI/180)
+  let cap = req.body.capacity;
+  console.log("lat "+lat1+" lon "+lon1);/////////////////////////
+  truckDB.find({date:req.body.date},function(err,locations){
+    const R = 6371;
+    var x1 = R * Math.cos(lat1) * Math.cos(lon1)
+    var y1 = R * Math.cos(lat1) * Math.sin(lon1);
+    var z1 = R * Math.sin(lat1)
     locations.forEach(function(location){
-      let lat2 = location.longitude
-      let lon2 = location.latitude
-      // console.log(lat1);
-      // console.log(lon1);
-      var R = 6371; // Radius of the earth in km
-      var dLat = (lat2 - lat1)*(Math.PI/180); // deg2rad below
-      var dLon = (lon2 - lon1)*(Math.PI/180);
-      //console.log(dLat);
-      var a =
-        Math.sin(dLat / 2) * Math.sin(dLat / 2) +
-        Math.cos((lat1)*(Math.PI/180)) * Math.cos((lat2)*(Math.PI/180)) *
-        Math.sin(dLon / 2) * Math.sin(dLon / 2);
-      var c = 2 * Math.atan2(Math.sqrt(a), Math.sqrt(1 - a));
-
-      var d = (R * c)%R; // Distance in km
-      console.log("dist: "+d);
+        // if(cap<0){
+        //   break;
+        // }
+        // cap = cap-location.capacity
+        let lat2 = (location.latitude)*(Math.PI/180)
+        let lon2 = (location.longitude)*(Math.PI/180)
+        var x2 = R*Math.cos(lat2)*Math.cos(lon2);
+        var y2 = R*Math.cos(lat2)*Math.sin(lon2);
+        var z2 = R*Math.sin(lat2);
+        var d = Math.sqrt((x1-x2)*(x1-x2) + (y1-y2)*(y1-y2) + (z1-z2)*(z1-z2))
+        console.log("dist "+d);////////////////////////////////
+        availableVehicle.push({dist:d,location:location})
+        console.log(availableVehicle);
     })
+  }).then((response) => {
+    //  console.log("availableVehicle", availableVehicle);
+      availableVehicle.sort(function(bandA,bandB){
+        //return a.dist>b.dist;
+        let comparison = 0;
+        if (bandA.dist > bandB.dist) {
+          comparison = 1;
+        } else if (bandA.dist < bandB.dist) {
+          comparison = -1;
+        }
+        return comparison;
+      });
+      res.render("nya",{vehicleDetails:availableVehicle});
   })
-  res.redirect("/")
+})
+
+
+app.post("/truck",function(req,res){
+  const truck = new truckDB({
+    longitude:req.body.longitude,
+    latitude:req.body.latitude,
+    capacity:req.body.capacity,
+    phone:req.body.phone,
+    date:req.body.date
+  })
+  truck.save();
+  res.redirect("/");
 })
